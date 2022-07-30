8d4f331a5264d5eba48b018c100e4375a0784a1b
Arif Niwang <arif.niwank1@gmail.com>
2022-07-30 19:04:28 +0700

    hide fun draising

--- resources/views/page/frontend/donation/donation.blade.php
+++ resources/views/page/frontend/donation/donation.blade.php
@@ -10,7 +10,6 @@
             background-image: url("{{ asset(\crocodicstudio\crudbooster\helpers\CRUDBooster::getSetting('support_background')) }}");
             min-height: 100vh
         }
-
     </style>
 @endpush
 
@@ -18,12 +17,12 @@
     <main id="main" class="d-flex flex-column align-items-center justify-content-center">
         <h4 class="title-big mb-5" data-aos="fade-down">SUPPORT US</h4>
         <div class="container-fluid d-flex flex-row flex-wrap align-items-center justify-content-center">
-            <a href="{{ webPath('support-us/fundraising') }}" class="menu-item" data-aos="fade-up">
+            {{-- <a href="{{ webPath('support-us/fundraising') }}" class="menu-item" data-aos="fade-up">
                 <div class="image">
                     <img src="{{ asset('vendor/front/assets/example/img/icon/ic_drainsing.png') }}">
                 </div>
                 <span class="title">FUNDRAISING</span>
-            </a>
+            </a> --}}
             <a href="{{ webPath('support-us/partner') }}" class="menu-item" data-aos="fade-up">
                 <div class="image">
                     <img src="{{ asset('vendor/front/assets/example/img/icon/ic_our_partner.png') }}">
